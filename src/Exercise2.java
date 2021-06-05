cae2065540d82124fee9ab8c65cc3d8a14954690
haranokengo <haranokengo@gmail.com>
2021-06-06 01:50:59 +0900

    完了

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,12 +1,13 @@
 
 public class Exercise2 {
 	public static void main(String[] args) {
-		int sum = 0;
+		int total = 0;
 		for (int i = 2; i <= 100; i++) {
 			if (i % 2 == 0) {
-				sum += i;
+				total += i;
 			}
 		}
-		System.out.println(sum);
+		System.out.println(total);
 	}
+
 }
